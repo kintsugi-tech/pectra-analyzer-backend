2d69ce9994569fe46debbd4dcc961f3c41c5115e
alessandromazza98 <alessandro98.mazza@gmail.com>
2025-04-26 16:33:10 +0200

    rpc now works

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,11 @@
 use alloy_consensus::Transaction;
 use alloy_primitives::{FixedBytes, hex::FromHex};
 use alloy_provider::{Provider, ProviderBuilder};
-use axum::{routing::get, Router};
+use axum::{Router, extract::Query, routing::get};
 use pectralizer::{BASE_STIPEND, compute_calldata_gas};
+use serde::Deserialize;
 use std::env;
 use url::Url;
-
-// #[tokio::main]
-// async fn main() {
-//     // load .env environment variables
-//     dotenv::dotenv().ok();
-//     // tx hash to analyze
-//     let tx_hash = "0x17ee587040c06bf85ee426eb975c737129d1082f2aaacc61abfedd0a9deb69a9";
-//     // ethereum infura endpoint
-//     let ethereum_infura_url = env::var("ETHEREUM_PROVIDER").unwrap();
-//     // create provider
-//     let provider = ProviderBuilder::new().on_http(Url::parse(&ethereum_infura_url).unwrap());
-//     // transform tx hash into a fixed bytes
-//     let tx_hash = FixedBytes::from_hex(tx_hash).unwrap();
-//     // get tx
-//     let tx = provider
-//         .get_transaction_by_hash(tx_hash)
-//         .await
-//         .unwrap()
-//         .unwrap();
-//     // get calldata
-//     let calldata = tx.input();
-//     // get receipt
-//     let receipt = provider
-//         .get_transaction_receipt(tx_hash)
-//         .await
-//         .unwrap()
-//         .unwrap();
-//     // get total gas used
-//     let gas_used = receipt.gas_used;
-//     println!("Gas used: {}", gas_used);
-//     // compute EIP-7623 calldata gas
-//     let eip_7623_calldata_gas = compute_calldata_gas(calldata);
-//     println!("EIP-7623 calldata gas: {}", eip_7623_calldata_gas);
-//     // check if EIP-7623 is effective
-//     let is_eip_7623_effective = eip_7623_calldata_gas + BASE_STIPEND > gas_used;
-//     println!("EIP-7623 is effective: {}", is_eip_7623_effective);
-// }
 
 #[tokio::main]
 async fn main() {
@@ -60,6 +24,39 @@ async fn root_handler() -> &'static str {
     "Welcome to the pectralizer api!"
 }
 
-async fn tx_handler(tx_hash: String) -> String {
-    
+async fn tx_handler(Query(query): Query<TxHashQuery>) -> String {
+    // ethereum infura endpoint
+    let ethereum_infura_url = env::var("ETHEREUM_PROVIDER").unwrap();
+    // create provider
+    let provider = ProviderBuilder::new().on_http(Url::parse(&ethereum_infura_url).unwrap());
+    // transform tx hash into a fixed bytes
+    println!("tx_hash: {}", query.tx_hash);
+    let tx_hash_bytes = FixedBytes::from_hex(&query.tx_hash).unwrap();
+    // get tx
+    let tx = provider
+        .get_transaction_by_hash(tx_hash_bytes)
+        .await
+        .unwrap()
+        .unwrap();
+    // get calldata
+    let calldata = tx.input();
+    // get receipt
+    let receipt = provider
+        .get_transaction_receipt(tx_hash_bytes)
+        .await
+        .unwrap()
+        .unwrap();
+    // get total gas used
+    let gas_used = receipt.gas_used;
+    // compute EIP-7623 calldata gas
+    let eip_7623_calldata_gas = compute_calldata_gas(calldata);
+    // check if EIP-7623 is effective
+    let is_eip_7623_effective = eip_7623_calldata_gas + BASE_STIPEND > gas_used;
+    is_eip_7623_effective.to_string()
+}
+
+/// Query parameters for the tx handler
+#[derive(Deserialize)]
+struct TxHashQuery {
+    tx_hash: String,
 }
